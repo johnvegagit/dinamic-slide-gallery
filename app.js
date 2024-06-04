355a97f9c1e26f29784f07c1e55861537fa3c4a3
johnvegagit <johnvegagit@outlook.com>
2024-06-03 19:40:22 -0600

    Update v1

---
+++ app.js
@@ -0,0 +1,62 @@
+const imgExten = [
+    'img-01.jpg',
+    'img-02.webp',
+    'img-03.webp',
+    'img-04.jpg',
+    'img-05.webp',
+];
+
+for (let i = 0; i < imgExten.length; i++) {
+    document.getElementById('dot-container').innerHTML += `
+        <span class="dot"></span>
+    `;
+}
+
+for (let i = 0; i < imgExten.length; i++) {
+    document.getElementById('img-container').innerHTML += `
+        <img src="http://localhost/public_html/dinamic-slide-gallery/images/${imgExten[i]}">
+    `;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let slideIndex = 0;
+    const imgCont = document.getElementById('img-container');
+    const slidesImg = imgCont.querySelectorAll('img');
+    const dotCont = document.getElementById('dot-container');
+    const dots = dotCont.querySelectorAll('.dot');
+    
+    function showSlides(n) {
+        if (n >= slidesImg.length) { slideIndex = 0 }
+        if (n < 0) { slideIndex = slidesImg.length - 1 }
+        
+        slidesImg.forEach((img, index) => {
+            img.style.display = (index === slideIndex) ? 'block' : 'none';
+        });
+        
+        dots.forEach((dot, index) => {
+            dot.classList.toggle('active', index === slideIndex);
+        });
+    }
+    
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    
+    function currentImg(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function autoSlides() {
+        showSlides(slideIndex += 1);
+    }
+    
+    document.querySelector('.prev').addEventListener('click', () => plusSlides(-1));
+    document.querySelector('.next').addEventListener('click', () => plusSlides(1));
+
+    dots.forEach((dot, index) => {
+        dot.addEventListener('click', () => currentImg(index));
+    });
+
+    setInterval(autoSlides, 3000);
+    showSlides(slideIndex);
+});
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="./app.js" defer></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <section>
+
+        <div id="img-container">
+
+            <button class="prev">❮</button>
+            <button class="next">❯</button>
+
+        </div>
+
+        <div id="dot-container">
+
+        </div>
+    </section>
+
+</body>
+
+</html>
